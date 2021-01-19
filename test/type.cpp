adba1335fc6d28986ca8920c37d6aedbf7d1b6d9
GDI512 <jakub.s7920@gmail.com>
2021-01-19 15:03:24 +0100

    Added more tests for the resource class

--- test/type.cpp
+++ test/type.cpp
@@ -1,18 +1,31 @@
 // ============================================================================
 //  Test file for test types provided by the library.
-//      1. Ensure resource reports its constructor call
-//      2. Ensure resource reports its destructor call
-//      3. Ensure resource reports an error on double destruction
+//      1. Ensure resource reports its constructor call (dynamic)
+//      2. Ensure resource reports its destructor call (dynamic)
+//      3. Ensure resource reports an error on double destruction (dynamic)
 //      4. Ensure resource reports an error on copy construction from an
-//         invalid object
+//         invalid object (dynamic)
 //      5. Ensure resource reports an error on copy assignment from an invalid
-//         object
+//         object (dynamic)
 //      6. Ensure resource reports an error on move construction from an
-//         invalid object
+//         invalid object (dynamic)
 //      7. Ensure resource reports an error on move assignment from an invalid
-//         object
+//         object (dynamic)
 //      8. Ensure resource reports an error on a destructor call into an
-//         already initialized memory area
+//         already initialized memory area (dynamic)
+//      9. Ensure resource reports its constructor call (automatic)
+//     10. Ensure resource reports its destructor call (automatic)
+//     11. Ensure resource reports an error on double destruction (automatic)
+//     12. Ensure resource reports an error on copy construction from an
+//         invalid object (automatic)
+//     13. Ensure resource reports an error on copy assignment from an invalid
+//         object (automatic)
+//     14. Ensure resource reports an error on move construction from an
+//         invalid object (automatic)
+//     15. Ensure resource reports an error on move assignment from an invalid
+//         object (automatic)
+//     16. Ensure resource reports an error on a destructor call into an
+//         already initialized memory area (automatic)
 // ============================================================================
 
 #include "common.hpp"
@@ -116,6 +129,92 @@ int main() {
         verifier state;
         new (objbuf.get()) resource();
         new (objbuf.get()) resource();
+        cassert(verifier::data().destroyed_count == 0);
+        cassert(verifier::data().constructed_count == 2);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 1);
+        cassert(verifier::data().operator_error_count == 0);
+    }
+    { // 9.
+        verifier state;
+        resource object;
+        cassert(verifier::data().destroyed_count == 0);
+        cassert(verifier::data().constructed_count == 1);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 0);
+        cassert(verifier::data().operator_error_count == 0);
+    }
+    { // 10.
+        verifier state;
+        resource object;
+        object.~resource();
+        cassert(verifier::data().destroyed_count == 1);
+        cassert(verifier::data().constructed_count == 1);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 0);
+        cassert(verifier::data().operator_error_count == 0);
+    }
+    { // 11.
+        verifier state;
+        resource object;
+        object.~resource();
+        object.~resource();
+        cassert(verifier::data().destroyed_count == 2);
+        cassert(verifier::data().constructed_count == 1);
+        cassert(verifier::data().destructor_error_count == 1);
+        cassert(verifier::data().constructor_error_count == 0);
+        cassert(verifier::data().operator_error_count == 0);
+    }
+    { // 12.
+        verifier state;
+        resource object;
+        object.~resource();
+        resource other(object);
+        cassert(verifier::data().destroyed_count == 1);
+        cassert(verifier::data().constructed_count == 2);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 1);
+        cassert(verifier::data().operator_error_count == 0);
+    }
+    { // 13.
+        verifier state;
+        resource object;
+        resource other;
+        object.~resource();
+        other = std::move(object);
+        cassert(verifier::data().destroyed_count == 1);
+        cassert(verifier::data().constructed_count == 2);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 0);
+        cassert(verifier::data().operator_error_count == 1);
+    }
+    { // 14.
+        verifier state;
+        resource object;
+        object.~resource();
+        resource other(std::move(object));
+        cassert(verifier::data().destroyed_count == 1);
+        cassert(verifier::data().constructed_count == 2);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 1);
+        cassert(verifier::data().operator_error_count == 0);
+    }
+    { // 15.
+        verifier state;
+        resource object;
+        resource other;
+        object.~resource();
+        other = std::move(object);
+        cassert(verifier::data().destroyed_count == 1);
+        cassert(verifier::data().constructed_count == 2);
+        cassert(verifier::data().destructor_error_count == 0);
+        cassert(verifier::data().constructor_error_count == 0);
+        cassert(verifier::data().operator_error_count == 1);
+    }
+    { // 16.
+        verifier state;
+        resource object;
+        new (&object) resource();
         cassert(verifier::data().destroyed_count == 0);
         cassert(verifier::data().constructed_count == 2);
         cassert(verifier::data().destructor_error_count == 0);
