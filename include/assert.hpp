749522bf4417da70e8c5c6cec85b15a2c3d3f78f
GDI512 <jakub.s7920@gmail.com>
2021-02-21 22:03:02 +0100

    Minor changes to assert.hpp

--- include/assert.hpp
+++ include/assert.hpp
@@ -4,13 +4,15 @@
 #include "state.hpp"
 #include "utility.hpp"
 
+#define scope __func__
+
 namespace test {
 
     inline auto check(bool value) noexcept -> bool {
         if (value) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
@@ -18,10 +20,10 @@ namespace test {
     auto check(T first, T last, U&& function) noexcept -> bool {
         for (; first != last; ++first) {
             if (!function(*first)) {
-                return core::registry::on_error(__func__);
+                return core::registry::on_error(scope);
             }
         }
-        return core::registry::on_success(__func__);
+        return core::registry::on_success(scope);
     }
 
     template <typename T, typename U, auto N>
@@ -30,18 +32,18 @@ namespace test {
         const auto last = core::end(array);
         for (; first != last; ++first) {
             if (!function(*first)) {
-                return core::registry::on_error(__func__);
+                return core::registry::on_error(scope);
             }
         }
-        return core::registry::on_success(__func__);
+        return core::registry::on_success(scope);
     }
 
     template <typename T, typename U>
     auto check_equal(const T& left, const U& right) noexcept -> bool {
         if (left == right) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
@@ -49,28 +51,28 @@ namespace test {
     auto check_equal(T first, T last, U other) noexcept -> bool {
         for (; first != last; ++first, ++other) {
             if (*first != *other) {
-                return core::registry::on_error(__func__);
+                return core::registry::on_error(scope);
             }
         }
-        return core::registry::on_success(__func__);
+        return core::registry::on_success(scope);
     }
 
     template <typename T, typename U, auto N>
     auto check_equal(const T (&array)[N], const U (&other)[N]) noexcept -> bool {
         for (auto i = 0; i < N; i++) {
             if (array[i] != other[i]) {
-                return core::registry::on_error(__func__);
+                return core::registry::on_error(scope);
             }
         }
-        return core::registry::on_success(__func__);
+        return core::registry::on_success(scope);
     }
 
     template <typename T, typename U>
     auto check_not_equal(const T& left, const U& right) noexcept -> bool {
         if (left != right) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
@@ -78,55 +80,55 @@ namespace test {
     auto check_not_equal(T first, T last, U other) noexcept -> bool {
         for (; first != last; ++first, ++other) {
             if (*first != *other) {
-                return core::registry::on_success(__func__);
+                return core::registry::on_success(scope);
             }
         }
-        return core::registry::on_error(__func__);
+        return core::registry::on_error(scope);
     }
 
     template <typename T, typename U, auto N>
     auto check_not_equal(const T (&array)[N], const U (&other)[N]) noexcept -> bool {
         for (auto i = 0; i < N; i++) {
             if (array[i] != other[i]) {
-                return core::registry::on_success(__func__);
+                return core::registry::on_success(scope);
             }
         }
-        return core::registry::on_error(__func__);
+        return core::registry::on_error(scope);
     }
 
     template <typename T, typename U>
     auto check_less(const T& left, const U& right) noexcept -> bool {
         if (left < right) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
     template <typename T, typename U>
     auto check_not_less(const T& left, const U& right) noexcept -> bool {
         if (left >= right) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
     template <typename T, typename U>
     auto check_greater(const T& left, const U& right) noexcept -> bool {
         if (left > right) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
     template <typename T, typename U>
     auto check_not_greater(const T& left, const U& right) noexcept -> bool {
         if (left <= right) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
@@ -134,9 +136,9 @@ namespace test {
     auto check_throws(T&& function, U&&... args) noexcept -> bool {
         try {
             function(core::forward<U>(args)...);
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         } catch (...) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         }
     }
 
@@ -144,27 +146,27 @@ namespace test {
     auto check_nothrows(T&& function, U&&... args) noexcept -> bool {
         try {
             function(core::forward<U>(args)...);
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } catch (...) {
-            return core::registry::on_error(__func__);
+            return core::registry::on_error(scope);
         }
     }
 
     template <typename T, typename U>
     auto check_sorted(T first, T last, U&& compare) noexcept -> bool {
         if (first == last) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
             auto previous = first;
             if (++first == last) {
-                return core::registry::on_success(__func__);
+                return core::registry::on_success(scope);
             } else {
                 for (; first != last; ++first, ++previous) {
                     if (!compare(*previous, *first)) {
-                        return core::registry::on_error(__func__);
+                        return core::registry::on_error(scope);
                     }
                 }
-                return core::registry::on_success(__func__);
+                return core::registry::on_success(scope);
             }
         }
     }
@@ -174,17 +176,19 @@ namespace test {
         auto first = core::begin(array);
         const auto last = core::end(array);
         if (N <= 1) {
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         } else {
             for (; first != last - 1; ++first) {
                 if (!compare(*first, *(first + 1))) {
-                    return core::registry::on_error(__func__);
+                    return core::registry::on_error(scope);
                 }
             }
-            return core::registry::on_success(__func__);
+            return core::registry::on_success(scope);
         }
     }
 
 }
+
+#undef scope
 
 #endif
